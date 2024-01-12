2739e6945f6e7c8ef2d0589de6289fe627814897
Crow Rising <miscbyrd@gmail.com>
2024-01-12 16:48:53 -0700

    test: add tests for hexagram model

--- app/models/hexagram.rb
+++ app/models/hexagram.rb
@@ -1,2 +1,9 @@
 class Hexagram < ApplicationRecord
+  validates :title, presence: true
+  validates :oracle, presence: true
+  validates :summary, presence: true
+  validates :judgment, presence: true
+  validates :judgment_sum, presence: true
+  validates :image, presence: true
+  validates :image_sum, presence: true
 end
--- spec/models/hexagram_spec.rb
+++ spec/models/hexagram_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Hexagram, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:oracle) }
+    it { should validate_presence_of(:summary) }
+    it { should validate_presence_of(:judgment) }
+    it { should validate_presence_of(:judgment_sum) }
+    it { should validate_presence_of(:image) }
+    it { should validate_presence_of(:image_sum) }
+  end
 end

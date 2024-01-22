8ebad12621bac6bf36d73c20c21d9554c0acaa17
Andrew Bingham <andrewgbingham@gmail.com>
2024-01-22 16:45:34 -0700

    feat: rename welcome to pages and and functions to hexagram controller

--- app/controllers/hexagrams_controller.rb
+++ app/controllers/hexagrams_controller.rb
@@ -1,5 +1,15 @@
 class HexagramsController < ApplicationController
+
+  def index
+    @hexagrams = Hexagram.all
+  end
+
   def show
     @hexagram = Hexagram.find(params[:id])
   end
+
+  def random
+    redirect_to hexagram_path(Hexagram.all.sample.id)
+  end
+
 end
